1a6ea8a006c7705141a3acd33ef6bd2106a6ed08
Shuxiang Yang <yangphysics@gmail.com>
2018-06-11 07:22:15 +0800

    06-integer-positive

--- 06-interger-positive.rb
+++ 06-interger-positive.rb
@@ -2,9 +2,23 @@
 
 
 print "请输入一个整数，然后按 Enter: "
-x = gets
+x = gets.to_i
 
-# ....
+if x>0
+    pm = '正数'
+elsif x<0
+    pm = '负数'
+else x>0
+    pm = '零'
+end
 
-puts "这个数是_____ (正数或零或负数)"
-puts "这个数是_____ (偶数或奇数)"
+if x/2 == 0
+    oe = '偶数'
+else x>0
+    oe = '奇数'
+end
+
+
+
+puts "这个数是#{pm}"
+puts "这个数是#{oe}"
