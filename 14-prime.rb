983be62e3f90f091d24b9ae2a93b8b1e9d0f1bf1
Shuxiang Yang <yangphysics@gmail.com>
2018-06-11 19:57:24 +0800

    14-prime

--- 14-prime.rb
+++ 14-prime.rb
@@ -1,7 +1,16 @@
 # 输入一个数字 N，请检查是不是质数
 
 def is_prime(n)
-# ....
+flag = true
+i = 2
+while i<n do
+  if n%i==0
+     flag = false
+     break
+  end
+  i += 1
+end
+flag
 end
 
 print "请输入数字 N，然后按 Enter: "
