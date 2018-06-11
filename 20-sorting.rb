9d6b42347cba5deeb16361b29f7b6d96fc411973
Shuxiang Yang <yangphysics@gmail.com>
2018-06-12 07:53:22 +0800

    20-sorting.rb

--- 20-sorting.rb
+++ 20-sorting.rb
@@ -1,11 +1,13 @@
 # 承上题，请排序并去除重复的数字
 # Hint: 可用 arr.sort 排序，和 arr.uniq 去除重复
 
-def filter_even(arr)
-  #...
+require_relative '19-filter'
+
+def filter_even1(arr)
+  filter_even(arr).sort().uniq()
 end
 
 arr =  [7, 68, 42, 46, 9, 91, 77, 46, 86, 1]
 
 
-puts "________" # 应该是 [42, 46, 68, 86]
+puts "#{filter_even1(arr)}" # 应该是 [42, 46, 68, 86]
