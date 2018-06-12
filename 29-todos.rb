dca36d9eeeb4621a92821d706a5891ccfb3899e7
Yang Shuxiang <yangphysics@gmail.com>
2018-06-12 11:18:06 +0800

    29-todos.rb

--- 29-todos.rb
+++ 29-todos.rb
@@ -17,14 +17,30 @@ while (true)
 
   if command == "add"
     print "请输入代办事项: "
-    # ...
+    thing = gets.chomp
+    if todos.include?(thing)
+        puts "代办事项 is already included"
+    else
+        todos << thing
+    end
   elsif command == "remove"
     print "请输入要删除的编号: "
-    # ...
+    idx = puts.to_i
+    if idx > todos.length
+        print " input index too large "
+    elsif idx < 0
+        print " negative index is not allowed"
+    else
+        todos.delete_at(idx)
+    end
   elsif command == "save"
     puts "存盘离开"
 
-    # ...
+    File.open('todos_new.txt', 'w') do |f| 
+        todos.each do |thing| 
+            f.write("#{thing}\n") 
+        end
+    end
     break;
   else
     puts "看不懂，请再输入一次"
