37a25d8f3c0f603e762bdefdbe1dea1e46f95851
Shuxiang Yang <yangphysics@gmail.com>
2018-06-12 08:06:53 +0800

    22-missing.rb

--- 22-missing.rb
+++ 22-missing.rb
@@ -1,7 +1,19 @@
 # 给定一阵列内含数字，请输出 0~9 中不见的数字
 
 def find_missing(arr)
-  # ...
+  i = 0
+  arr = arr.sort().uniq()
+  j = 0
+  arr1 = []
+  while i<10
+    if i != arr[j]
+      arr1 << i
+    else
+      j += 1
+    end
+    i += 1
+  end
+  arr1
 end
 
 answer = find_missing( [2,2,1,5,8,4] )
