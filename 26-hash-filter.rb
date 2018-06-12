4303f59b0577a162bf6186c181c7420b9997acd9
Yang Shuxiang <yangphysics@gmail.com>
2018-06-12 10:31:42 +0800

    26-hash-filter.rb

--- 26-hash-filter.rb
+++ 26-hash-filter.rb
@@ -8,9 +8,30 @@ arr = [
   { "name" => "Vincent", "age" => 6 },
 ]
 
-# ....
+arr1 = []
+arr.each do |x|
+    if x["age"]>=18
+        arr1 << x
+    end
+end
 
-puts "所有成年人，并由小到大: _________"
+i = 0
+while i<arr1.length() do
+    j = i+1
+    while j<arr1.length() do
+        puts "  #{i}  #{j}  "
+        if arr1[i]["age"]>arr1[j]["age"]
+            puts arr1
+            puts " swap "
+            arr1[i],arr1[j] = arr1[j],arr1[i]
+            puts arr1
+        end
+        j += 1
+    end
+    i += 1
+end
+
+puts "所有成年人，并由小到大: #{arr1}"
 
 # 答案应该是
 #[
